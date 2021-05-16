3caa567fddc91d49ef21540a4ec3dc3e3b08facd
EryiZ <2659186332@qq.com>
2021-05-16 08:41:58 +0800

    comment

--- ansible/run-nectar.sh
+++ ansible/run-nectar.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
-
-. ./openrc.sh; ansible-playbook mrc.yaml
+#. ./pt-59642-openrc.sh; ansible-playbook mrc.yaml
+#. ./unimelb-comp90024-2021-grp-57-openrc.sh; ansible-playbook mrc.yaml
+#ansible-playbook couchdb.yml
+#ansible-playbook test.yml
