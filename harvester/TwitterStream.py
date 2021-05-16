05ae1129499b4d76b4b27e60da256fef990b636e
ll An <llan@192-168-1-132.tpgi.com.au>
2021-05-17 00:55:55 +1000

    harvester

---
+++ harvester/TwitterStream.py
@@ -0,0 +1,84 @@
+import TwitterAPI
+import couchdb
+from datetime import datetime
+import time
+#before get the value, check not none
+#get doc["place"]["name"]
+#check doc["place"] is not none
+def get_data(response: str,path:list) -> str:
+    r=response
+    for p in path:
+        if r and p in r:
+            r=r[p]
+        else:
+            return None
+    return r
+
+#Check if the key of dict doc has a value
+#such as place, coordinates
+def check_none(doc: dict, key: str, value: str):
+    if value:
+        doc[key]=value
+    else: doc[key]=None
+
+#build a dict doc for upload to couchdb later
+#contains: _id, user_id, post_text, location, location_fullname, coodinates
+def get_result(response: dict) -> dict:
+    
+    doc = dict()
+    doc["_id"] = str(response["id"])
+    #print(doc["_id"],'doc["_id"]')
+    d=get_data(response,["user","id"])
+    doc["user_id"] = d
+    doc["post_text"] = response['text']
+    doc["time"] = new_datetime = datetime.strftime(datetime.strptime(response["created_at"],
+            '%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d %H:%M:%S')
+    check_none(doc,"location", get_data(response,["place","name"]) )
+    check_none(doc,"location_fullname", get_data(response,["place","full_name"]))
+    check_none(doc,"coordinates",get_data(response,["coordinates","coordinates"]))
+    return doc
+
+#use filter stream select location
+def filter_stream(api,db,keywords,boxes):
+    count=0
+    #track_term delimited by ","
+    TRACK_TERM=",".join(keywords)
+    #construct the request to twitter
+    request_map={'locations': boxes}
+    if TRACK_TERM=="":
+        print("filter without keyword")
+    else:
+        #if the keywords are not empty, set it in the request
+        request_map['track']=TRACK_TERM
+    while True:
+        try:
+            r = api.request('statuses/filter', request_map)
+            for response in r:
+                #use dict doc to store results
+                doc=get_result(response)
+                if not doc:                 
+                    print ("didn't get ideal response, try again")
+                    continue                
+                db.save(doc)
+                count+=1
+                if count==1:
+                    print(doc)
+                if count % 100 ==0:
+                    print (count)
+        #Ignore the existence of the same id
+        except couchdb.http.ResourceConflict:
+            pass
+        except TwitterAPI.TwitterError.TwitterRequestError as e:
+            if e.status_code < 500:
+                time.sleep(60)
+                raise
+            else:
+                time.sleep(60)
+                pass
+        except TwitterAPI.TwitterError.TwitterConnectionError:
+            time.sleep(60)
+            pass
+        
+
+
+
---
+++ harvester/config.py
@@ -0,0 +1,12 @@
+consumer_key = "2KYzayBTxaykRUmDrDgYWeW2D"
+consumer_secret = "cKOLznQWR94ET3JcFGotUbvKJnckBVdwZazY9jhnp1eSNXSLTE" 
+access_token = "1254697631600660480-12cqVeP49fcoL5RfmaDoeW0DjlRjvf" 
+access_token_secret = "N0gfaDsqvwPlLVlRjGT0OSfWiJnMI9BRC9sSf6XfpBo8R" 
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAJa9PAEAAAAAfXT1jiOVkwBjum1E1H5q7T%2F5nIE%3DpLoB8Be1X1EhUG1sZQrrRZ5tOsd1CDXPkFAi7vuXJc4b0fPMNc"
+db_url = "http://admin:123456@127.0.0.1:5984" 
+db_name = "tweet_melb" 
+keywords = [""] 
+#box_au = "112.5,-37.5,154.1,-12.8" 
+box_melb = "143.947,-38.468,145.066,-36.882"
+#box_syd = "150.6,-33.8,151.4,-33.3"
+#https://www.mapdevelopers.com/draw-circle-tool.php
