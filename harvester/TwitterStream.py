e89e3a53d92b1b17b2359f7898100794284c66ab
angaoanzj <angaoanzj@163.com>
2021-05-17 15:30:19 +1000

    stream

--- harvester/TwitterStream.py
+++ harvester/TwitterStream.py
@@ -2,6 +2,7 @@ import TwitterAPI
 import couchdb
 from datetime import datetime
 import time
+import json
 #before get the value, check not none
 #get doc["place"]["name"]
 #check doc["place"] is not none
@@ -33,24 +34,33 @@ def get_result(response: dict) -> dict:
     doc["post_text"] = response['text']
     doc["time"] = new_datetime = datetime.strftime(datetime.strptime(response["created_at"],
             '%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d %H:%M:%S')
+    check_none(doc,"user_location", get_data(response,["user","location"]))
     check_none(doc,"location", get_data(response,["place","name"]) )
     check_none(doc,"location_fullname", get_data(response,["place","full_name"]))
     check_none(doc,"coordinates",get_data(response,["coordinates","coordinates"]))
+    check_none(doc,"hashtags",get_data(response,["entities","hashtags"]))
     return doc
 
+def write_json(json_str):
+    with open('melb_json.json','a+') as f:
+        f.write(json_str)
+
+
+
 #use filter stream select location
-def filter_stream(api,db,keywords,boxes):
+##def filter_stream(api,db,keywords,boxes):
+def filter_stream(api,keywords,boxes):
     count=0
     #track_term delimited by ","
     TRACK_TERM=",".join(keywords)
     #construct the request to twitter
-    request_map={'locations': boxes}
+    request_map={'locations': boxes,"lang": "en","retweeted":"false"}
     if TRACK_TERM=="":
         print("filter without keyword")
     else:
         #if the keywords are not empty, set it in the request
         request_map['track']=TRACK_TERM
-    while True:
+    while 1:
         try:
             r = api.request('statuses/filter', request_map)
             for response in r:
@@ -58,16 +68,20 @@ def filter_stream(api,db,keywords,boxes):
                 doc=get_result(response)
                 if not doc:                 
                     print ("didn't get ideal response, try again")
-                    continue                
-                db.save(doc)
+                    continue 
+                json_doc = json.dumps(doc)          
+                
+                write_json(json_doc)
+                ##db.save(doc)
                 count+=1
-                if count==1:
-                    print(doc)
+                #if count==1:
+                    #break
+                    #print(doc)
                 if count % 100 ==0:
                     print (count)
         #Ignore the existence of the same id
-        except couchdb.http.ResourceConflict:
-            pass
+        ##except couchdb.http.ResourceConflict:
+        ##    pass
         except TwitterAPI.TwitterError.TwitterRequestError as e:
             if e.status_code < 500:
                 time.sleep(60)
