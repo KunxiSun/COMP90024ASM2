eb313d480d3622e1268fb9b558119176e3e8b5f1
KunxiSun <sunkunxi@qq.com>
2021-05-23 11:34:08 -0700

    backgroud harvester

--- harvester/src/main.py
+++ harvester/src/main.py
@@ -54,13 +54,10 @@ def search():
 
 
 if __name__ == "__main__":
-    t1 = threading.Thread(target=search)
-    t2 = threading.Thread(target=streaming)
+    t1 = threading.Thread(target=search, daemon=True)
+    t2 = threading.Thread(target=streaming, daemon=True)
   
     # starting threads
     t1.start()
     t2.start()
-  
-    # joining threads
-    t1.join()
-    t2.join()
+
