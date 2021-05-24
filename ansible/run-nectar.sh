95ce6779ece13d1e09fd46d6fee50c8c62e9a30a
EryiZ <2659186332@qq.com>
2021-05-24 22:09:40 +0800

    create dynamic inventory

--- ansible/run-nectar.sh
+++ ansible/run-nectar.sh
@@ -1,4 +1,4 @@
 #. ./pt-59642-openrc.sh; ansible-playbook mrc.yaml
-. ./unimelb-comp90024-2021-grp-57-openrc.sh; ansible-playbook --ask-become-pass mrc.yaml
+. ./unimelb-comp90024-2021-grp-57-openrc.sh; ansible-playbook --ask-become-pass mrc.yaml -i inventory/hosts.ini
 #ansible-playbook couchdb.yml
 #ansible-playbook test.yml
